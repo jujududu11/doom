a7d042bc3943d697514aab08af812f55ea31a53d
jujududu11 <86871498+jujududu11@users.noreply.github.com>
2023-07-27 23:59:56 +0200

    Create dom.py

---
+++ dom.py
@@ -0,0 +1,16 @@
+def tire(x,y,r):
+  pass
+def map(x,y):
+  distance=[[0]*x]*y
+def mur(mur)
+  for i in gange(len[mur]-1):
+    mumu=mur[i]
+    mumu0=mumu[0] # point 1
+    mumu1=mumu[1] # point 2
+    mumu2=mumu[2] # texture
+    mo0= mumu0.split("/")
+    mo1= mumu1.split("/")
+    if mo0[0] == mo1[0]():
+      for w in gange(mo1[0]-mo0[0])
+      re=mo0[0] + w
+      distance[mo1[1]][re] = i
