1dec30500b3e11475771739ba9a04e8377b79708
jujududu11 <86871498+jujududu11@users.noreply.github.com>
2023-07-28 17:28:09 +0200

    Update dom.py

--- dom.py
+++ dom.py
@@ -4,23 +4,29 @@ from tkinter import * as tk
 def play():
   pass
 def option():
-  pass
+  root.quit()
+  retoure = Tk.Botton(root,text="retour",command=init)
+  root = Tk()
+
+
+  
+  root.mainloop()
 def quit():
   root.quit()
 
+def init():
+  root = Tk()
+  bouton1 = PhotoImage(file = r"\image\play.png")
+  bouton2 = PhotoImage(file = r"\image\option.png")
+  bouton3 = PhotoImage(file = r"\image\quit.png")
 
-root = Tk()
-bouton1 = PhotoImage(file = r"\image\play.png")
-bouton2 = PhotoImage(file = r"\image\option.png")
-bouton3 = PhotoImage(file = r"\image\quit.png")
+  play = Tk.Button(root,image=bouton1,command=play)
+  play.pack
 
-play = Tk.Button(root,bouton1,command=play)
-play.pack
+  option = Tk.Button(root,image=bouton2,command=option)
+  option.pack
 
-option = Tk.Button(root,bouton2,command=option)
-option.pack
+  quit = Tk.Button(root,image=bouton3,command=quit)
+  quit.pack
 
-quit = Tk.Button(root,bouton3,command=quit)
-quit.pack
-
-root.mainloop()
+  root.mainloop()
