88086e92388a5acce51eee8ad505d3dd236dbcc4
jujududu11 <86871498+jujududu11@users.noreply.github.com>
2023-07-28 18:02:26 +0200

    Update dom.py

--- dom.py
+++ dom.py
@@ -1,11 +1,21 @@
 import motor.py as mo
 from tkinter import * as tk
-
+mod=[["",""]*100]
+def mod():
+  root2 = Tk()
+  liste = Listbox(root2,)
+  for i in gange():
+    liste.insert(i+1, mod[0][i])
+  liste.pack()
+  root2.mainloop()
 def play():
   pass
 def option():
   root.quit()
-  retoure = Tk.Botton(root,text="retour",command=init)
+  mod = Tk.Botton(root,text="mod"command=mod)
+  mod.pack()
+  retour = Tk.Botton(root,text="retour",command=init)
+  retour.pack()
   root = Tk()
 
 
@@ -21,12 +31,12 @@ def init():
   bouton3 = PhotoImage(file = r"\image\quit.png")
 
   play = Tk.Button(root,image=bouton1,command=play)
-  play.pack
+  play.pack()
 
   option = Tk.Button(root,image=bouton2,command=option)
-  option.pack
+  option.pack()
 
   quit = Tk.Button(root,image=bouton3,command=quit)
-  quit.pack
+  quit.pack()
 
   root.mainloop()
