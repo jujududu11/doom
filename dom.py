ba997e2051733b5f5cbf63a551ff7b3c2a891600
jujududu11 <86871498+jujududu11@users.noreply.github.com>
2023-07-28 16:25:25 +0200

    Update dom.py

--- dom.py
+++ dom.py
@@ -1,16 +1,20 @@
-def tire(x,y,r):
+def tire(x,y,r): # pour tirer avec une arme a distence r pour la rotation vert la gauche =0 vert le haut =90 vert le bas =-90 a droite =-180
   pass
-def map(x,y):
-  distance=[[0]*x]*y
-def mur(mur)
-  for i in gange(len[mur]-1):
-    mumu=mur[i]
-    mumu0=mumu[0] # point 1
-    mumu1=mumu[1] # point 2
-    mumu2=mumu[2] # texture
-    mo0= mumu0.split("/")
-    mo1= mumu1.split("/")
-    if mo0[0] == mo1[0]():
-      for w in gange(mo1[0]-mo0[0])
-      re=mo0[0] + w
-      distance[mo1[1]][re] = i
+def map(x,y): # doner x pour la auteur y pour de gauche a droite
+  distance=[[0]*x]*y #créer la map avec un double liste
+def mur(mur: # faire tout les mur
+  for i in gange(len[mur]-1): #faite tout les mur
+    mumu=mur[i] # choisie les mur
+    mumu0=mumu[0] # point 1 du point 1
+    mumu1=mumu[1] # point 2 au point 2 fait un mur
+    mumu2=mumu[2] # texture donne la texture du mur
+    mo0= mumu0.split("/") # mo0[0]= point 1 x  mo0[1]= point 1 y
+    mo1= mumu1.split("/") # mo0[0]= point 2 x  mo0[1]= point 2 y
+    if mo0[0] == mo1[0](): # is point 1x = point 1y 
+      for w in gange(mo1[0]-mo0[0]) # toute les point du mur
+      re=mo0[0] + w # y du mur
+      distance[mo1[1]][re] = i # nom du mur
+def mob(ia,x,y): # ia pour l'inteligent x pour gauche a droite y de haute en bas
+  mob=[]*len(mob)+1 # agrandit la liste de mob
+  mob[len(mob)+1] = [ia,x,y] # ecrit la liste de mob avec x y est le code de sont ia
+  
