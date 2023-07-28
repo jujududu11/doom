84ed7b00dc03c080c715cfc6a8b40ba81da8052c
jujududu11 <86871498+jujududu11@users.noreply.github.com>
2023-07-28 17:04:06 +0200

    Update dom.py

--- dom.py
+++ dom.py
@@ -1,4 +1,26 @@
 import motor.py as mo
 from tkinter import * as tk
+
+def play():
+  pass
+def option():
+  pass
+def quit():
+  root.quit()
+
+
 root = Tk()
-play = Tk.Button()
+bouton1 = PhotoImage(file = r"\image\play.png")
+bouton2 = PhotoImage(file = r"\image\option.png")
+bouton3 = PhotoImage(file = r"\image\quit.png")
+
+play = Tk.Button(root,bouton1,command=play)
+play.pack
+
+option = Tk.Button(root,bouton2,command=option)
+option.pack
+
+quit = Tk.Button(root,bouton3,command=quit)
+quit.pack
+
+root.mainloop()
